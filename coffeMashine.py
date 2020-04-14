2ac3aefa86c6536807c913a86c5b2b968e86be8f
Gezubov <40934590+Gezubov@users.noreply.github.com>
2020-04-14 09:19:40 +0300

    addind loops

--- coffeMashine.py
+++ coffeMashine.py
@@ -1,6 +1,9 @@
-supplies = {"water": 1200, "milk": 540, "beans": 120, "cups": 9, "money": 550}
+supplies = {"water": 400, "milk": 540, "beans": 120, "cups": 9, "money": 550}
+espresso = {"water": 250, "milk": 0, "beans": 16, "cups": 1, "money": 4}
+latte = {"water": 350, "milk": 75, "beans": 20, "cups": 1, "money": 7}
+cappuchino = {"water": 200, "milk": 100, "beans": 12, "cups": 1, "money": 6}
 
-def stats():
+def remaining():
     print(f"""The coffee machine has:
     {supplies["water"]} of water
     {supplies["milk"]} of milk
@@ -8,54 +11,60 @@ def stats():
     {supplies["cups"]} of disposable cups
     {supplies["money"]} of money""")
 
-def espresso():
-    supplies["water"] -= 250
-    supplies["beans"] -= 16
-    supplies["money"] += 4
-    supplies["cups"] -= 1
-
-def latte():
-    supplies["water"] -= 350
-    supplies["milk"] -= 75
-    supplies["beans"] -= 20
-    supplies["money"] += 7
-    supplies["cups"] -= 1
-
-def cappuchino():
-    supplies["water"] -= 200
-    supplies["milk"] -= 100
-    supplies["beans"] -= 12
-    supplies["money"] += 6
-    supplies["cups"] -= 1
+def makingCoffee(recepie):
+    if supplies["water"] < recepie["water"]:
+        print("Sorry, not enough water")
+    elif supplies["milk"] < recepie["milk"]:
+        print("Sorry, not enough milk")
+    elif supplies["beans"] < recepie["beans"]:
+        print("Sorry, not enough beans")
+    elif supplies["cups"] < recepie["cups"]:
+        print("Sorry, not enough disposable cups")
+    else:
+        print("I have enough resources, making you a coffee!")
+        supplies["water"] -= recepie["water"]
+        supplies["milk"] -= recepie["milk"]
+        supplies["beans"] -= recepie["beans"]
+        supplies["money"] += recepie["money"]
+        supplies["cups"] -= recepie["cups"]
 
 def buy():
-    coffeeChoice = int(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n"))
-    if coffeeChoice == 1:
-        espresso()
-    if coffeeChoice == 2:
-        latte()
-    if coffeeChoice == 3:
-        cappuchino()
+    while True:
+        coffeeChoice = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:\n")
+        if coffeeChoice == "back":
+            break
+        if coffeeChoice == "1":
+            makingCoffee(espresso)
+            break
+        if coffeeChoice == "2":
+            makingCoffee(latte)
+            break
+        if coffeeChoice == "3":
+            makingCoffee(cappuchino)
+            break
 
 def fill():
     supplies["water"] += int(input("How many ml of water do you want to add:\n"))
     supplies["milk"] += int(input("How many ml of milk do you want to add:\n"))
     supplies["beans"] += int(input("How many grams of coffee beans do you want to add:\n"))
-    supplies["cups"] += int(input("How many disposable cups of coffee do you want to add:\n"))
+    supplies["cups"] += int(input("How many disposable cups do you want to add:\n"))
 
 def take():
     print(f'I gave you ${supplies["money"]}')
     supplies["money"] = 0
 
 def coffeMachine():
-    stats()
-    action = input("Write action (buy, fill, take):\n")
-    if action == "buy":
-        buy()
-    if action == "fill":
-        fill()
-    if action == "take":
-        take()
-    stats()
+    while True:
+        action = input("Write action (buy, fill, take, remaining, exit):\n")
+        if action == "exit":
+            break
+        if action == "remaining":
+            remaining()
+        if action == "buy":
+            buy()
+        if action == "fill":
+            fill()
+        if action == "take":
+            take()
 
 coffeMachine()
